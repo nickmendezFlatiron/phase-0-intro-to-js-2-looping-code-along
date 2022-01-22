9e8e68c554001c7c6dc2c018033cb0e3f5434db7
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-01-22 16:34:26 -0500

    commit tree e26a9d3d212cbd703fc7c425e676b098d1e2f723

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // Code your solutions in this file
-function writeCards(array,event){
+function writeCards (array,event){
     const newArray = [];
-    for(let i = 0, n = array.length; i < n ; i++){
-       newArray[i] = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+    for (let i = 0, n = array.length ; i < n; i++) {
+        newArray[i] = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
     }
     return newArray;
 }
 
-function countDown (int) {
-    while (int >= 0) {
-        console.log(int);
-        int--;
+function countDown(number){
+    while (number >= 0 ) {
+        console. log (number);
+        number-- ;
     }
 }
